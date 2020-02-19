24492a6f232a5589a634a711d26fe560cde02dfb
Shevchik <Shev4ik.den@gmail.com>
2020-02-19 04:27:21 +0300

    Support escaping spaces in command args

--- src/org/hurricanegames/commandlib/commands/BukkitCommandExecutor.java
+++ src/org/hurricanegames/commandlib/commands/BukkitCommandExecutor.java
@@ -1,7 +1,9 @@
 package org.hurricanegames.commandlib.commands;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,7 +33,7 @@ public class BukkitCommandExecutor implements CommandExecutor, TabCompleter {
 			if (permission != null) {
 				command.getHelper().validateHasPermission(sender, permission);
 			}
-			command.handleCommand(new CommandContext(command.getHelper().getMessages(), sender, label, args));
+			command.handleCommand(new CommandContext(command.getHelper().getMessages(), sender, label, splitEscaped(args)));
 		} catch (CommandResponseException e) {
 			sender.sendMessage(e.getMessage());
 		}
@@ -44,10 +46,48 @@ public class BukkitCommandExecutor implements CommandExecutor, TabCompleter {
 			if (permission != null) {
 				command.getHelper().validateHasPermission(sender, permission);
 			}
-			return command.getAutoComplete(new CommandContext(command.getHelper().getMessages(), sender, label, args));
+			return
+				command.getAutoComplete(new CommandContext(command.getHelper().getMessages(), sender, label, args)).stream()
+				.map(BukkitCommandExecutor::escape)
+				.collect(Collectors.toList());
 		} catch (CommandResponseException e) {
 			return Collections.emptyList();
 		}
+	}
+
+	protected static String[] splitEscaped(String[] args) {
+		ArrayList<String> arglist = new ArrayList<>();
+
+		StringBuilder argbuilder = new StringBuilder();
+		boolean escape = false;
+		for (char c : String.join(" ", args).toCharArray()) {
+			if (escape) {
+				escape = false;
+				argbuilder.append(c);
+			} else if (c == ' ') {
+				arglist.add(argbuilder.toString());
+				argbuilder.setLength(0);
+			} else if (c == '\\') {
+				escape = true;
+			} else {
+				argbuilder.append(c);
+			}
+		}
+		if (argbuilder.length() > 0) {
+			arglist.add(argbuilder.toString());
+		}
+		return arglist.toArray(new String[0]);
+	}
+
+	protected static String escape(String string) {
+		StringBuilder result = new StringBuilder();
+		for (char c : string.toCharArray()) {
+			if ((c == '\\') || (c == ' ')) {
+				result.append('\\');
+			}
+			result.append(c);
+		}
+		return result.toString();
 	}
 
 }
